37df7e2b414dad673028d105230224e446506a87
Joao S O Bueno <gwidion@gmail.com>
2019-02-17 23:50:08 -0300

    Uses key position to determine color, multiply fill

--- typewriter.py
+++ typewriter.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2
 
+import colorsys
 import random
 
 from gimpfu import *
@@ -7,12 +8,56 @@ from gimpfu import *
 import gtk
 import gtk.gdk
 
+numbersrow = "0123456789"
+toprow = "qwertyuiop"
+middlerow = "asdfghjkl"
+bottomrow = "zxcvbnm,."
+
+keys = [numbersrow, toprow, middlerow, bottomrow]
+def get_hsl(key):
+    pos = None
+    for i, row in enumerate(keys):
+        if row.find(key) != -1:
+            pos = row.find(key)
+            break
+    else:
+        return 0, ord(key[0]) % 2, 0
+    hue = pos * 1.0 / len(row)
+    if i == 0:
+        lit = 0.9
+        sat = 1
+    elif i == 1:
+        lit = 0.6
+        sat = 0.75
+    elif i == 2:
+        lit = 0.5
+        sat = 1
+    else:
+        lit = 0.3
+        sat = 1
+    return hue, sat, lit
+
+def hsl_to_rgb(H, S, L):
+    r_, g_, b_ = colorsys.hls_to_rgb(H, L, S)
+    r, g, b = (int(comp * 255) for comp in (r_, g_, b_))
+    return r, g, b
+
 
 def paint(color, drawing):
-    pdb.gimp_ellipse_select(image, x, y, size, vsize, CHANNEL_OP_REPLACE, True, True, 12)
+    pdb.gimp_ellipse_select(image, x, y, size, vsize, CHANNEL_OP_REPLACE, True, True, 20)
     pdb.gimp_context_set_foreground(color)
     if drawing:
-        pdb.gimp_edit_fill(drw, FILL_FOREGROUND)
+        # pdb.gimp_edit_fill(drw, FILL_FOREGROUND)
+        pdb.gimp_edit_bucket_fill(
+            drw,
+            BUCKET_FILL_FG,
+            LAYER_MODE_MULTIPLY,
+            100,
+            255,
+            False,
+            0,
+            0
+        )
     pdb.gimp_displays_flush()
 
 
@@ -41,11 +86,12 @@ def keychain(window, event):
         y -= size
 
     cval = (val - 32) & 0x7
-    color = bool(cval & 0x4) * 255, bool(cval & 0x2) * 255, bool(cval & 0x1) * 255
+    # color = bool(cval & 0x4) * 255, bool(cval & 0x2) * 255, bool(cval & 0x1) * 255
+    color = hsl_to_rgb(*get_hsl(name))
 
     paint(color, drawing)
     if drawing:
-        x += size
+        x += size // 2
         if x > image.width:
             y += vsize
             x = 0
